059f79a321e2d051fdf786f697a99fcb3bdaa606
Chris Fisher <chris.i.fisher@gmail.com>
2024-07-26 17:09:11 +1200

    wip

--- src/types.ts
+++ src/types.ts
@@ -90,7 +90,7 @@ export interface EnrollVerifiedAuthenticatorResponse {
 
 export interface ValidateChallengeRequest {
   token: string;
-  action: string;
+  action?: string;
   userId?: string;
 }
 
